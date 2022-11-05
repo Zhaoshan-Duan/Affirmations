0461e7c145b7b5d81fcd534af64f3b39cc3baf4a
Zhaoshan-Duan <joshua.duan47@gmail.com>
2022-11-04 21:46:58 -0700

    Add images and styles

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
@@ -21,6 +22,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -28,7 +30,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
@@ -38,6 +40,7 @@ class ItemAdapter(private val context: Context, private val dataset: List<Affirm
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     /**
--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,4 +1,9 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 // an object instance would represent one affirmation and contains the resource ID of the string
-data class Affirmation(val stringResourceId: Int) {}
+
+// Use annotation to prevent called could accidentially pass in the argument in the wrong order
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int)
