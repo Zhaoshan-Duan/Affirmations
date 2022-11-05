31054fac93192d0dcbbf2a3cfd0332cbd7c5fddd
Zhaoshan-Duan <joshua.duan47@gmail.com>
2022-11-04 18:03:52 -0700

    Initial commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.example.affirmations.model
+
+// an object instance would represent one affirmation and contains the resource ID of the string
+data class Affirmation(val stringResourceId: Int) {}
